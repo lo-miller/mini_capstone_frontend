702d5b1423405a84f7326215d40f4e44ed0e7f4d
Lauren Miller <lomiller0913@gmail>
2021-04-07 19:32:32 -0400

    initial commit. add productsIndex view file

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "All Products",
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {},
+  },
+};
+</script>
+
