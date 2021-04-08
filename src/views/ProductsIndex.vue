2fe137de21f06aed9d4b8475954b59178368d3d0
Lauren Miller <lomiller0913@gmail>
2021-04-07 21:10:42 -0400

    builds out index view

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="home">
+  <div class="products-index">
     <h1>{{ message }}</h1>
+    <div class="row">
+      <div class="col-sm-4" v-for="product in products">
+        <div class="card">
+          <img style="width: 200px" v-bind:src="product.images[0]&&product.images[0].url" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h3 class="card-title">{{product.name}}</h3>
+            <p class="card-text">Price: {{product.price}}</p>
+            <p class="card-text">{{product.description}}</p>
+            <a href="#" class="btn btn-primary">Product details</a>
+          </div>
+        </div>
+      </div>
+      <!-- <p>Description: {{product.description}}</p> -->
+      <!-- <p>{{product.images}}</p> -->
+      <!-- <p>{{product.images[0].url}}</p> -->
+    </div>
 
   </div>
 </template>
@@ -9,6 +25,8 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
@@ -20,7 +38,12 @@ export default {
     this.indexProducts();
   },
   methods: {
-    indexProducts: function () {},
+    indexProducts: function () {
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
   },
 };
 </script>
